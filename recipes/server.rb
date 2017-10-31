c1b951982cc4955f743520e12dce995f3cebbb1f
rstatsinger <rstatsinger@gmail.com>
2017-10-30 23:01:37 -0700

    Initial Commit

---
+++ recipes/server.rb
@@ -0,0 +1,65 @@
+
+
+# server recipe
+
+# execute sudo yum install java-1.7.0-openjdk-devel
+
+package 'java-1.7.0-openjdk-devel'
+
+user 'tomcat' do
+	manage_home false
+	home '/opt/tomcat'
+end
+
+group 'tomcat' do
+	members 'tomcat'
+	action :create
+end
+
+# link in the lab instructions is dead - I found the distribution here:
+# wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz  
+#
+# extract to /opt/tomcat
+#
+# sample war link ok
+# https://github.com/johnfitzpatrick/certification-workshops/blob/master/Tomcat/sample.war
+
+
+remote_file 'apache-tomcat-8.0.33.tar.gz' do
+	source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz'
+end
+
+directory '/opt/tomcat' do
+	action [:create]
+	group 'tomcat'
+end
+
+
+# TODO - make this stuff idempotent
+#
+execute 'tar -xvf apache-tomcat-8.0.33.tar.gz -C /opt/tomcat --strip-components=1'
+execute 'chgrp -R tomcat /opt/tomcat/conf'
+execute 'chmod g+rwx /opt/tomcat/conf'
+execute 'chmod g+r /opt/tomcat/conf/*'
+
+execute 'chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs'
+
+# This one is idempotent - will have to revise the others
+
+directory '/opt/tomcat/conf' do
+	mode '0755'
+end
+
+# template for the tomcat service file
+# sudo vi /etc/systemd/system/tomcat.service
+
+template '/etc/systemd/system/tomcat.service' do
+	source 'tomcatService.erb'
+end
+
+execute 'systemctl daemon-reload'
+
+service 'tomcat' do
+	action [:start, :enable]
+end
+
---
+++ test/smoke/default/default_test.rb
@@ -0,0 +1,63 @@
+# # encoding: utf-8
+
+# Inspec test for recipe tomcat::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+#unless os.windows?
+  # This is an example test, replace with your own test.
+#  describe user('root'), :skip do
+#    it { should exist }
+#  end
+#end
+
+# This is an example test, replace it with your own test.
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+
+describe command('curl http://localhost:8080') do
+	its('stdout') { should match /Tomcat/ }
+end
+
+describe package('java-1.7.0-openjdk-devel') do
+	it { should be_installed }
+end
+
+describe group('tomcat') do
+	it { should exist }
+end
+
+# directories
+
+describe directory('/opt/tomcat') do
+	it { should exist }
+end
+
+describe directory('/opt/tomcat/conf') do
+	it { should exist }
+	its('mode') { should cmp '0755' }
+end
+
+%w[ webapps work temp logs ].each do |path|
+
+	describe directory("/opt/tomcat/#{path}") do
+		it { should exist }
+		its('owner') { should eq 'tomcat' }
+	end
+end
+
+# user
+
+describe user('tomcat') do
+	it { should exist }
+	its('groups') { should include 'tomcat' }
+	its('home') { should eq '/opt/tomcat' }
+end
+
+describe file ('/etc/systemd/system/tomcat.service') do
+	it { should exist }
+end
+
+
