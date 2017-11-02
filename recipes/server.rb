1d410cef1f0624b5b52401b97fbd48ce8b9f24fa
rstatsinger <rstatsinger@gmail.com>
2017-11-01 19:51:51 -0700

    this tim for sure

--- recipes/server.rb
+++ recipes/server.rb
@@ -23,9 +23,9 @@ end
 directory '/opt/tomcat' do
 	action [:create]
 	group 'tomcat'
-	not_if do
-		File.exist?('/opt/tomcat')
-	end
+ 	not_if do
+ 		File.exist?('/opt/tomcat')
+ 	end
 end
 
 # link in the lab instructions is dead - I found the distribution here:
@@ -38,9 +38,9 @@ end
 
 remote_file '/opt/tomcat/apache-tomcat-8.0.33.tar.gz' do
 	source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.33/bin/apache-tomcat-8.0.33.tar.gz'
-	not_if do
-		File.exist?('/opt/tomcat/apache-tomcat-8.0.33.tar.gz')
-	end
+ 	not_if do
+ 		File.exist?('/opt/tomcat/apache-tomcat-8.0.33.tar.gz')
+ 	end
 end
 
 
@@ -49,34 +49,32 @@ end
 # assume if the conf subdir exists, we've already untarred
 #
 execute 'tar -xvf /opt/tomcat/apache-tomcat-8.0.33.tar.gz -C /opt/tomcat --strip-components=1' do
-	not_if do
-		File.exist?('/opt/tomcat/conf')
-	end
+ 	not_if do
+ 		File.exist?('/opt/tomcat/conf')
+ 	end
 end
 
 
 execute 'chgrp -R tomcat /opt/tomcat/conf' do
- 	only_if { node['confGroupOK'] == 0 }
-	node.override['confGroupOK'] = 1
+ 	only_if { (Etc.getgrgid(File.stat('/opt/tomcat/conf').gid)).name != 'tomcat' }
 end
 
 
 execute 'chmod g+rwx /opt/tomcat/conf' do
- 	only_if { node['confPermOK'] == 0 }
-	node.override['confPermOK'] = 1
+ 	only_if { File.stat('/opt/tomcat/conf').mode != '770' }
 
 end
 
 
 execute 'chmod g+r /opt/tomcat/conf/*' do
- 	only_if { node['confPermAllOK'] == 0 }
-	node.override['confPermAllOK'] = 1
+ 	only_if{  File.stat('/opt/tomcat/conf/server.xml').mode != '710' }
+ 	node.override['confPermAllOK'] = 1
 
 end
  
 execute 'chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs' do
- 	only_if { node['dirOwnersOK'] == 0 }
-	node.override['dirOwnersOK'] = 1
+ 	only_if { Etc.getpwuid(File.stat('/opt/tomcat/webapps').uid).name != 'tomcat' }
+	#node.override['dirOwnersOK'] = 1
 end
 
 
@@ -100,14 +98,12 @@ end
 template '/opt/tomcat/conf/server.xml' do
  	source 'tomcatServerXML.erb'
    	owner 'tomcat'
-   	# notifies :run, 'execute[daemonReload]',:immediately
    	notifies :restart, 'service[tomcat]',:immediate
 end
 
 # execute 'systemctl daemon-reload'
 execute 'daemonReload' do
 	command 'systemctl daemon-reload'
-	# action :nothing
 end
 
 # subscribes properties for state changes on tomcat.service and server.xml
